959d37824fc8a577425e6aaa9481e664a3cb5f3e
Jake Gillis <jaques.gillis@gmail.com>
2015-11-15 20:26:24 -0500

    Added function to prompt user for max number and check that it is valid, and moved old code into functions

--- js/script.js
+++ js/script.js
@@ -1,18 +1,38 @@
 $(document).ready (function() {
 
-	for (i = 1; i < 101; i++) {
-		if ((i % 3 == 0) && (i % 5 == 0)) {
-			$(".numlist").append("<p>FizzBuzz</p>");
-		}
-		else if (i % 3 == 0) {
-			$(".numlist").append("<p>Fizz</p>");
-		}
-		else if (i % 5 == 0) {
-			$(".numlist").append("<p>Buzz</p>");
-		}
-		else {
-			$(".numlist").append("<p>" + i + "</p>");
+	function FizzBuzz(top) {
+		
+		for (i = 1; i <= top; i++) {
+			/* copying the nice check function in Thinkful lesson */
+			function check(n) {
+				msg = '';
+				if (n % 3 == 0) {
+					msg += "Fizz";
+				}
+				if (n % 5 == 0) {
+					msg += "Buzz";
+				}
+				return msg || n;
+			}
+
+			$(".numlist").append("<p>" + check(i) + "</p>");
 		}
 	}
+
+	function getNum() {
+		var rawmax = prompt("Enter a number: ");
+		/* 
+		 * The first condition ensures rawmax is not a decimal number.
+		 * Since NaN !== NaN, second condition ensures that rawmax is a number 
+		 */
+		if (((rawmax * 100) % 100 > 0) || (parseInt(rawmax) !== parseInt(rawmax))) {
+			rawmax = prompt("Enter a WHOLE NUMBER: ");
+		}
+		Max = parseInt(rawmax, 10);
+	}
+	
+	getNum();
+	FizzBuzz(Max);
+
 });
 
